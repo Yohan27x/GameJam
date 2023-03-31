8e912dc8ca571b1fc195a722d9edfabaa2f81048
Yohan27x <yoyo1999sb@gmail.com>
2023-03-31 10:09:16 +0200

    Added level transition prototype

---
+++ GameJam/test.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+var camera_pos = [Vector2(126,98),Vector2(380,98),Vector2(350,350)]
+var camera_pos_index = 1
+onready var timer = $Timer
+onready var camera = $Camera2D
+
+var level_finish = false
+
+func _process(delta):
+	
+	if(level_finish == true):
+		var new_camera_pos = camera_pos[camera_pos_index]
+		camera.position = lerp(camera.position, new_camera_pos, 0.02)
+		camera.zoom = lerp(camera.zoom, Vector2(3,3), 0.1)
+
+	if(level_finish == false):
+		camera.zoom = lerp(camera.zoom, Vector2(1,1), 0.1)
+			
+
+func _on_Timer_timeout():
+	level_finish = true
+	
+func _on_Area2D_area_entered(area):
+	level_finish = false
+	camera_pos_index += 1
+	timer.start(3)
